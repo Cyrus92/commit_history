7a3fcd5415a6149f8fd91f59532279248c52fcc9
Cyrus92 <codjo.cyrus@gmail.com>
2022-05-25 09:20:39 +0100

    Test and ensure that the CommitsPage component is rendered

---
+++ src/tests/CommitsPage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import CommitsPage from '../views/CommitsPage';
+describe("CommitsPage component", () => {
+
+    it("should render", () => {
+        const wrapper = shallow(<CommitsPage />);
+    });
+    
+    
+    
+
+});
---
+++ src/views/CommitsPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class CommitsPage extends React.Component {
+  render() {
+    return <div />;
+  }
+}
